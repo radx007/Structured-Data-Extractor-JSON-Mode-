ef24ddae404f29994f47e3ca73252ce48a695fac
Mohammed Ramzi Abdallah <radx.abdel@gmail.com>
2026-06-02 03:37:01 +0100

    fixes

--- app/config/settings.py
+++ app/config/settings.py
@@ -13,27 +13,27 @@ class Settings:
     MAX_UPLOAD_FILES = int(os.getenv("MAX_UPLOAD_FILES",4))
 
     # LLM config
-    LLAMA_CHAT_URL = os.getenv("LLAMA_CHAT_URL")
-    LLM_MODEL_NAME = os.getenv("LLM_MODEL_NAME")
+    LLAMA_CHAT_URL = os.getenv("LLAMA_CHAT_URL", "http://localhost:8000/v1/chat/completions")
+    LLM_MODEL_NAME = os.getenv("LLM_MODEL_NAME", "llama3.2:3b-instruct-q4_K_M")
 
     # OCR config
-    N_GPU_LAYERS = int(os.getenv("N_GPU_LAYERS"))
-    CONTEXT_SIZE = int(os.getenv("CONTEXT_SIZE"))
+    N_GPU_LAYERS = int(os.getenv("N_GPU_LAYERS"), 99)
+    CONTEXT_SIZE = int(os.getenv("CONTEXT_SIZE"), 4096)
     
-    LLAMA_CLI_PATH = os.getenv("LLAMA_CLI_PATH")
-    LLAMA_DIR = os.getenv("LLAMA_DIR")
-    GLM_OCR_MODEL = os.getenv("GLM_OCR_MODEL")
-    GLM_MMPROJ = os.getenv("GLM_MMPROJ")
+    LLAMA_CLI_PATH = os.getenv("LLAMA_CLI_PATH", "C:/llama/llama-mtmd-cli.exe" )
+    LLAMA_DIR = os.getenv("LLAMA_DIR", "C:/llama")
+    GLM_OCR_MODEL = os.getenv("GLM_OCR_MODEL", "models/GLM-OCR.Q4_K_M.gguf")
+    GLM_MMPROJ = os.getenv("GLM_MMPROJ", "models/mmproj-GLM-OCR-Q4_K_M.gguf")
 
     # llama.cpp server config
-    LLAMA_SERVER_EXECUTABLE = os.getenv("LLAMA_SERVER_EXECUTABLE",)
-    LLAMA_SERVER_MODEL_PATH = os.getenv("LLAMA_SERVER_MODEL_PATH",)
-    LLAMA_SERVER_WORKDIR = os.getenv("LLAMA_SERVER_WORKDIR",)
-    LLAMA_SERVER_HOST = os.getenv("LLAMA_SERVER_HOST")
-    LLAMA_SERVER_PORT = os.getenv("LLAMA_SERVER_PORT")
-    LLAMA_SERVER_CONTEXT_SIZE = os.getenv("LLAMA_SERVER_CONTEXT_SIZE")
-    LLAMA_SERVER_GPU_LAYERS = os.getenv("LLAMA_SERVER_GPU_LAYERS")
-    LLAMA_SERVER_FLASH_ATTENTION = os.getenv("LLAMA_SERVER_FLASH_ATTENTION")
-    LLAMA_SERVER_THREADS = os.getenv("LLAMA_SERVER_THREADS")
+    LLAMA_SERVER_EXECUTABLE = os.getenv("LLAMA_SERVER_EXECUTABLE", "C:/llama/llama-server.exe")
+    LLAMA_SERVER_MODEL_PATH = os.getenv("LLAMA_SERVER_MODEL_PATH","models/Llama-3.2-3B-Instruct-Q4_K_M.gguf")
+    LLAMA_SERVER_WORKDIR = os.getenv("LLAMA_SERVER_WORKDIR","C:/llama")
+    LLAMA_SERVER_HOST = os.getenv("LLAMA_SERVER_HOST","127.0.0.1")
+    LLAMA_SERVER_PORT = os.getenv("LLAMA_SERVER_PORT", 8000)
+    LLAMA_SERVER_CONTEXT_SIZE = os.getenv("LLAMA_SERVER_CONTEXT_SIZE", 4096)
+    LLAMA_SERVER_GPU_LAYERS = os.getenv("LLAMA_SERVER_GPU_LAYERS", 32)
+    LLAMA_SERVER_FLASH_ATTENTION = os.getenv("LLAMA_SERVER_FLASH_ATTENTION", "on")
+    LLAMA_SERVER_THREADS = os.getenv("LLAMA_SERVER_THREADS", 6)
 
 settings = Settings()
